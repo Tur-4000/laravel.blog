0e2c8cc9063fe4f3c55d228a2ad336e1d0cf6701
Turbanov <tur.4000@gmail.com>
2021-01-11 21:55:31 +0200

    use sluggable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
+    use Sluggable;
 
     public function tags()
     {
@@ -17,5 +19,19 @@ class Post extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
