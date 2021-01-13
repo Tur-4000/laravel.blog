e8908337b6abd73efa45658f343d06ce4a829f9e
Turbanov <tur.4000@gmail.com>
2021-01-14 00:17:35 +0200

    предзагрузка

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -21,7 +21,7 @@ class PostController extends Controller
     public function index()
     {
         $title = 'Статьи';
-        $posts = Post::paginate();
+        $posts = Post::with('category', 'tags')->paginate();
 
         return view('admin.posts.index', compact('title', 'posts'));
     }
